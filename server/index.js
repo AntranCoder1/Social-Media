d113d3cd8a1cbb3ed8323c0fb0b875ea716c3be6
thanhantran1 <thanhantran21@gmail.com>
2021-11-04 16:32:15 +0700

    Start working on the server side

---
+++ server/index.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const express = require('express');
+const helmet = require('helmet');
+const app = express();
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(
+            `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@social-media.pgrhv.mongodb.net/Social-Media?retryWrites=true&w=majority`, 
+            {
+                useCreateIndex: true,
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+				useFindAndModify: false
+            }
+        )
+        console.log('MongoDB connected');
+    } catch (error) {
+        console.log(error.message);
+        process.exit(1);
+    }
+}
+
+connectDB();
+
+// middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/", (req, res) => {
+    res.send("hello");
+})
+
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+})
