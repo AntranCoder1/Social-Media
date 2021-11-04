90e5194c9189979fb5d756e1fc7b2b994062e8c3
thanhantran1 <thanhantran21@gmail.com>
2021-11-04 17:54:50 +0700

    add function login in routers/auth.router.js on server side

--- server/routers/auth.router.js
+++ server/routers/auth.router.js
@@ -28,6 +28,22 @@ router.post('/register', async (req, res) => {
     }
 })
 
+// @router api/auth/login
+// @desc POST user
+// @access Public
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        !user && res.status(500).json({ success: false, message: 'User not found' });
 
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        !validPassword && res.status(400).json({ success: false, message: 'Wrong password' });
+
+        res.status(200).json({ success: true, message: 'User logged in successfully', user });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+})
 
 module.exports = router;
