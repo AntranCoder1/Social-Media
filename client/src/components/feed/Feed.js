a738234e7ccee1c791b813de30476c0d6328b257
thanhantran1 <thanhantran21@gmail.com>
2021-11-08 16:54:48 +0700

    add post layout on client side

--- client/src/components/feed/Feed.js
+++ client/src/components/feed/Feed.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Post from '../post/Post';
 import Share from '../share/Share';
 import './Feed.css';
 
 const Feed = () => {
     return (
         <div className="feed">
-            <Share />
+            <div className="feedWrapper">
+                <Share />
+                <Post />
+            </div>
         </div>
     )
 }
---
+++ client/src/components/post/Post.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { MoreVert } from "@material-ui/icons";
+import './Post.css';
+
+const Post = () => {
+    return (
+        <div className="post">
+            <div className="postWrapper">
+                <div className="postTop">
+                    <div className="postTopLeft">
+                        <img 
+                            className="postProfileImg"
+                            src="/assets/person/1.jpeg"
+                            alt=""
+                        />
+                        <span className="postUsername">
+                            An
+                        </span>
+                        <span className="postDate">11/08/2021</span>
+                    </div>
+                    <div className="postTopRight">
+                        <MoreVert />
+                    </div>
+                </div>
+                <div className="postCenter">
+                    <span className="postText">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</span>
+                    <img 
+                        src="" 
+                        className="postImg"
+                        alt=""
+                    />
+                </div>
+                <div className="postBottom">
+                    <div className="postBottomLeft">
+                        <img className="likeIcon" src="assets/like.png" alt="" />
+                        <img className="likeIcon" src="assets/heart.png" alt="" />
+                        <span className="postLikeCounter"> people like it</span>
+                    </div>
+                    <div className="postBottomRight">
+                        <span className="postCommentText">comments</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Post
