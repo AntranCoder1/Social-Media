8f7c2853ddb2956a9891caef6cf3aa430c6bfa88
thanhantran1 <thanhantran21@gmail.com>
2021-11-10 18:01:44 +0700

    add user profile page layout on client side

---
+++ client/src/pages/profile/Profile.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Feed from '../../components/feed/Feed';
+import RightBar from '../../components/rightBar/RightBar';
+import SideBar from '../../components/sideBar/SideBar';
+import TopBar from '../../components/topBar/TopBar';
+import './Profile.css';
+
+const Profile = () => {
+    return (
+        <>
+            <TopBar />
+            <div className="profile">
+                <SideBar />
+                <div className="profileRight">
+                    <div className="profileRightTop">
+                        <div className="profileCover">
+                            <img 
+                                src="assets/post/3.jpeg" 
+                                className="profileCoverImg"
+                                alt=""
+                            />
+                            <img 
+                                src="assets/person/7.jpeg" 
+                                className="profileUserImg"
+                                alt=""
+                            />
+                        </div>
+                        <div className="profileInfo">
+                            <h4 className="profileInfoName">Safak Kocaoglu</h4>
+                            <span className="profileInfoDesc">Hello my friends!</span>
+                        </div>
+                    </div>
+                    <div className="profileRightBottom">
+                        <Feed />
+                        <RightBar />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Profile
