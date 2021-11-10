ea76afce382ff51befddfaa52f5899f8c7d01d7c
thanhantran1 <thanhantran21@gmail.com>
2021-11-10 19:19:08 +0700

    updated file in rightbar.js on client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import Home from './pages/home/Home';
-import Login from './pages/login/Login';
-import Profile from './pages/profile/Profile';
-import Register from './pages/register/Register';
 
 function App() {
   return (
-    <Register />
+    <Home />
   );
 }
 
