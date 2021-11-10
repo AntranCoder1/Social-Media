827e5a665db5b1b412ac810c6783b22dea382702
thanhantran1 <thanhantran21@gmail.com>
2021-11-10 19:07:46 +0700

    add register page on client side

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Profile from './pages/profile/Profile';
+import Register from './pages/register/Register';
 
 function App() {
   return (
-    <Login />
+    <Register />
   );
 }
 
