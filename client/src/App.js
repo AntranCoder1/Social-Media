f95abfc265521d0787288bb4d120e8a7988decbb
thanhantran1 <thanhantran21@gmail.com>
2021-11-10 18:53:51 +0700

    add login page layout on client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Home from './pages/home/Home';
+import Login from './pages/login/Login';
 import Profile from './pages/profile/Profile';
 
 function App() {
   return (
-    <Profile />
+    <Login />
   );
 }
 
