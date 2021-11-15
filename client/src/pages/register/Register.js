67edcb4f1cdc9b16fcd49880d16b17c30fdc4116
thanhantran1 <thanhantran21@gmail.com>
2021-11-15 15:38:54 +0700

    Register system with mongoDB using useHistory

--- client/src/pages/register/Register.js
+++ client/src/pages/register/Register.js
@@ -1,27 +1,81 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useRef } from 'react';
+import { useHistory } from 'react-router';
 import './Register.css';
 
 const Register = () => {
+
+    const username = useRef();
+    const email = useRef();
+    const password = useRef();
+    const passwordAgain = useRef();
+
+    const history = useHistory();
+
+    const handleClick = async (e) => {
+        e.preventDefault();
+        if (passwordAgain.current.value !== password.current.value) {
+            passwordAgain.current.setCustomValidity("Password don't match!");
+        } else {
+            const user = {
+                username: username.current.value,
+                email: email.current.value,
+                password: password.current.value,
+            }
+            try {
+                await axios.post('/auth/register', user);
+                history.push('/login');
+            } catch (error) {
+                console.log(error); 
+            }
+        }
+    }
+
     return (
         <div className="login">
             <div className="loginWrapper">
                 <div className="loginLeft">
-                <h3 className="loginLogo">Allgrow labo</h3>
-                <span className="loginDesc">
-                    Connect with friends and the world around you on Allgrow labo.
-                </span>
+                    <h3 className="loginLogo">Allgrow labo</h3>
+                    <span className="loginDesc">
+                        Connect with friends and the world around you on Allgrow labo.
+                    </span>
                 </div>
                 <div className="loginRight">
-                <div className="loginBox">
-                    <input placeholder="Username" className="loginInput" />
-                    <input placeholder="Email" className="loginInput" />
-                    <input placeholder="Password" className="loginInput" />
-                    <input placeholder="Password Again" className="loginInput" />
-                    <button className="loginButton">Sign Up</button>
-                    <button className="loginRegisterButton">
-                        Log into Account
-                    </button>
-                </div>
+                    <form className="loginBox" onSubmit={handleClick}>
+                        <input 
+                            placeholder="Username" 
+                            ref={username} 
+                            className="loginInput" 
+                            required
+                            type="text"
+                        />
+                        <input 
+                            placeholder="Email" 
+                            ref={email} 
+                            className="loginInput" 
+                            required
+                            type="email"
+                        />
+                        <input 
+                            placeholder="Password" 
+                            ref={password} 
+                            className="loginInput"
+                            required
+                            type="password"
+                            minLength="6"
+                         />
+                        <input 
+                            placeholder="Password Again" 
+                            ref={passwordAgain} 
+                            className="loginInput"
+                            required
+                            type="password"
+                         />
+                        <button className="loginButton" type="submit">Sign Up</button>
+                        <button className="loginRegisterButton">
+                            Log into Account
+                        </button>
+                    </form>
                 </div>
             </div>
         </div>
